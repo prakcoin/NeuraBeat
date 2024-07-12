2b9d43567d34393bd4dfccb46371880772260d72
prakcoin <prakhar2@gmail.com>
2024-07-12 17:12:27 -0500

    Further refactoring/organization

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import Flask, request, render_template
 from torchvision.transforms import v2
 from model.model import ClassificationModel, EmbeddingModel
 from utils.db import insert_embedding, embedding_exists, retrieve_similar_embeddings
+from utils.utils import preprocess, load_model
 
 app = Flask(__name__)
 
@@ -19,39 +20,6 @@ conn = psycopg2.connect(
 )
 
 class_names = ['Electronic', 'Experimental', 'Folk', 'Hip-Hop', 'Instrumental', 'International', 'Pop', 'Rock']
-
-def load_model(model_path, model_type):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    if model_type == 'classification':
-        model = ClassificationModel()
-    elif model_type == 'embedding':
-        model = EmbeddingModel()
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-    model.to(device)
-    model.eval()
-    return model
-
-def preprocess(song, sr=16000, n_mels=128, n_fft=2048, hop_length=512, mean=6.5304, std=11.8924):
-    chunk_duration = 3
-    chunk_length = sr * chunk_duration
-
-    song_waveform, w_sr = torchaudio.load(song)
-    song_waveform = T.Resample(orig_freq=w_sr, new_freq=sr)(song_waveform)
-    song_waveform = torch.mean(song_waveform, dim=0)
-    
-    song_len = len(song_waveform)
-    bound = rand.randint(0, song_len - chunk_length)
-    song_waveform = song_waveform[bound:bound + chunk_length]
-
-    song_waveform = song_waveform.unsqueeze(0)
-
-    mel_spec = T.MelSpectrogram(sample_rate=sr, n_mels=n_mels, n_fft=n_fft, hop_length=hop_length)(song_waveform)
-    log_mel_spec =  T.AmplitudeToDB()(mel_spec)
-    mel_spec_tensor = log_mel_spec.unsqueeze(0)
-    mel_spec_tensor = v2.Compose([v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
-                                  v2.Normalize((mean,), (std,))])(mel_spec_tensor)
-
-    return mel_spec_tensor
 
 @app.route('/process_file', methods=['POST'])
 def process_file():
---
+++ utils/utils.py
@@ -0,0 +1,39 @@
+import torch
+import random as rand
+import torchaudio
+import torchaudio.transforms as T
+from torchvision.transforms import v2
+from model.model import ClassificationModel, EmbeddingModel
+
+def load_model(model_path, model_type):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if model_type == 'classification':
+        model = ClassificationModel()
+    elif model_type == 'embedding':
+        model = EmbeddingModel()
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    model.to(device)
+    model.eval()
+    return model
+
+def preprocess(song, sr=16000, n_mels=128, n_fft=2048, hop_length=512, mean=6.5304, std=11.8924):
+    chunk_duration = 3
+    chunk_length = sr * chunk_duration
+
+    song_waveform, w_sr = torchaudio.load(song)
+    song_waveform = T.Resample(orig_freq=w_sr, new_freq=sr)(song_waveform)
+    song_waveform = torch.mean(song_waveform, dim=0)
+    
+    song_len = len(song_waveform)
+    bound = rand.randint(0, song_len - chunk_length)
+    song_waveform = song_waveform[bound:bound + chunk_length]
+
+    song_waveform = song_waveform.unsqueeze(0)
+
+    mel_spec = T.MelSpectrogram(sample_rate=sr, n_mels=n_mels, n_fft=n_fft, hop_length=hop_length)(song_waveform)
+    log_mel_spec =  T.AmplitudeToDB()(mel_spec)
+    mel_spec_tensor = log_mel_spec.unsqueeze(0)
+    mel_spec_tensor = v2.Compose([v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
+                                  v2.Normalize((mean,), (std,))])(mel_spec_tensor)
+
+    return mel_spec_tensor
