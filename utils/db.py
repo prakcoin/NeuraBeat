4160f07aec63cec866c8f190b5440e4e08d0d866
prakcoin <prakhar2@gmail.com>
2024-08-02 22:27:00 -0500

    Refactored db.py and removed unecessary code

--- utils/db.py
+++ utils/db.py
@@ -1,22 +1,9 @@
 import psycopg2
 import os
 import sys
+import boto3
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
-from model.model import EmbeddingModel
-import torchaudio.transforms as T
-from torchvision.transforms import v2
-from librosa.util import fix_length
-import torch
-import csv
-import boto3
-import torchaudio
-from collections import defaultdict
-
-mp3_data_path = '/mnt/c/Users/User/Documents/NeuraBeat/fma_small/'
-csv_path = '/mnt/c/Users/User/Documents/NeuraBeat/metadata/fma_metadata/tracks.csv'
-embedding_model_path = '../model/saved models/embedding_model.pt'
-bucket_name = 'neurabeat'
 
 conn = psycopg2.connect(
     dbname=os.getenv('DB_NAME'),
@@ -126,109 +113,3 @@ def upload_to_s3(file_path, bucket_name, object_name):
     except Exception as e:
         print(f"Error uploading {file_path} to S3: {e}")
         return None
-
-def create_file_genre_map(mp3_data_path, csv_path):
-    file_genre_map = {} 
-    track_ids = [file_name.split('.')[0].lstrip('0') for file_name in os.listdir(mp3_data_path) if file_name.endswith('.mp3')]
-
-    with open(csv_path, 'r') as csvfile:
-        csvreader = csv.reader(csvfile)
-        next(csvreader) 
-        next(csvreader)
-        next(csvreader)
-        for row in csvreader:
-            if row[0] in track_ids:
-                genre = row[40]
-                file_genre_map[row[0]] = genre
-    
-    return file_genre_map
-
-def insert_all_embeddings(model_path, mp3_data_path, file_genre_map, bucket_name, conn):
-    cur = conn.cursor()
-    genre_counts = defaultdict(int)
-    max_songs_per_genre = 990
-    target_sr = 16000
-    n_mels=128
-    n_fft=2048
-    hop_length=512
-    mean=6.5226
-    std=10.4655
-    
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = EmbeddingModel()
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-    model.to(device)
-    model.eval()
-
-    chunk_duration = 3
-    full_song_length = 27
-    num_chunks = full_song_length // chunk_duration
-
-    for mp3_file in os.listdir(mp3_data_path):
-        track_id = mp3_file.split('.')[0].lstrip('0')
-        genre = file_genre_map[track_id]
-        if genre_counts[genre] >= max_songs_per_genre:
-            continue
-
-        try:    
-            audio, sr = torchaudio.load(os.path.join(mp3_data_path, mp3_file))
-            resampled_audio = T.Resample(orig_freq=sr, new_freq=target_sr)(audio)
-            resampled_audio = torch.mean(resampled_audio, dim=0)
-            if (len(resampled_audio) / target_sr) < full_song_length:
-                print(f"Skipped short file: {mp3_file}")
-                continue
-            padded_audio = fix_length(resampled_audio, size=target_sr * full_song_length)
-
-            orig_chunk_length = sr * chunk_duration
-            chunk_length = target_sr * chunk_duration
-            for i in range(num_chunks):
-                start_sample = i * chunk_length
-                end_sample = start_sample + chunk_length
-
-                orig_start_sample = i * orig_chunk_length
-                orig_end_sample = orig_start_sample + orig_chunk_length 
-                if end_sample > len(padded_audio):
-                    break
-                orig_audio_chunk = audio[:, orig_start_sample:orig_end_sample].clone().detach()
-                audio_chunk = padded_audio[start_sample:end_sample].clone().detach().unsqueeze(0)
-
-                song_name = track_id + "_c" + str(i)
-                chunk_filename = f"{song_name}.wav"
-                chunk_filepath = os.path.join('/mnt/c/Users/User/Documents/NeuraBeat/fma_small chunks/', chunk_filename)
-                torchaudio.save(chunk_filepath, orig_audio_chunk, sr)
-
-                s3_url = upload_to_s3(chunk_filepath, bucket_name, chunk_filename)
-                if s3_url is None:
-                    print(f"Failed to upload {chunk_filename} to S3.")
-                    continue
-
-                mel_spec = T.MelSpectrogram(sample_rate=target_sr, n_mels=n_mels, n_fft=n_fft, hop_length=hop_length)(audio_chunk)
-                log_mel_spec = T.AmplitudeToDB()(mel_spec)
-                mel_spec_tensor = log_mel_spec.unsqueeze(0)
-                mel_spec_tensor = v2.Compose([v2.Resize((64, 47)),
-                                                v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
-                                                v2.Normalize((mean,), (std,))])(mel_spec_tensor)
-
-                with torch.no_grad():
-                    embedding = model(mel_spec_tensor)
-                embedding = embedding.flatten().detach().cpu().numpy().tolist()
-
-                cur.execute("""
-                    INSERT INTO song_embeddings (song_name, genre, s3_url, embedding)
-                    VALUES (%s, %s, %s, %s)
-                    ON CONFLICT (embedding) DO NOTHING;
-                """, (song_name, genre, s3_url, embedding))
-                conn.commit()
-                if cur.rowcount == 0:
-                    print("Skipped: Embedding already exists in the database.")
-                else:
-                    print("Inserted track", song_name)
-
-            genre_counts[genre] += 1
-
-        except Exception as e:
-            print(e)
-            print(f"Skipped corrupt file: {mp3_file}")
-
-    cur.close()
-    conn.close()
